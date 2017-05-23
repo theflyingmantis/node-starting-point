3c07bae6f1631fb476186f339e1f705f95d58a28
admin <admin@Abhinav-Rai.local>
2017-05-23 12:56:07 +0530

    sub,mul and div added

--- src/calculator.js
+++ src/calculator.js
@@ -2,8 +2,27 @@ function sum(a, b) {
   return a + b;
 }
 
+function subtract(a,b){
+  return a-b;
+}
+
+function divide(a,b)
+{
+	if(b===0)
+		return "BAD_INPUT";
+	return a/b;
+}
+
+function multiplication(a,b)
+{
+	return a*b;
+}
+
 module.exports = {
-  sum: sum 
+  sum: sum ,
+  sub: subtract,
+  div: divide,
+  mul: multiplication
 }
 
 
--- test/calculator-test.js
+++ test/calculator-test.js
@@ -1,9 +1,32 @@
 var assert = require('assert');
 var Calculator = require('../src/calculator');
 describe('Calculator', function() {
+  
   describe('sum', function() {
     it('should return 2 when inputs are 1,1', function() {
       assert.equal(2, Calculator.sum(1,1));
     });
   });
+
+  describe('subtract',function(){
+    it('should return 1 when inputs are 2,1',function(){
+    	assert.equal(1,Calculator.sub(2,1));
+    });
+  });
+
+  describe('divide',function(){
+  	it('should return 4 when inputs are 8,2',function(){
+  		assert.equal(4,Calculator.div(8,2));
+  	});
+  	it('should return error when inputs are 4,0',function(){
+  		assert.equal('BAD_INPUT',Calculator.div(4,0));
+  	});
+  });
+
+  describe('multiplication',function(){
+  	it('should return 6 when inputs are 3,2',function(){
+  		assert.equal(6,Calculator.mul(3,2));
+  	});
+  });
+
 });
